a8be5134f50103a6f0a54f041e5aaa7c0d2166d9
gymobi <dev@gymobi.com>
2012-01-31 19:29:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4fe7308eddbc6e03110ef63576fca0051c1af9892c1a98fbc9a8d575a8c000b7161ce13ff1cd9688e4f7171cfbfeb25096c93d9c6c94ee6d6fb3580a0ac86394'
